d26fd604cc749492a194349a4e6574ac9fa22f1c
mengjun <813935532@qq.com>
2019-03-06 19:34:47 +0800

    add mongodb.sh

---
+++ mongodb.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+touch /etc/yum.repos.d/mongodb-org-3.6.repo
+
+cat > /etc/yum.repos.d/mongodb-org-3.6.repo <<-EOF
+[mongodb-org-3.6]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/3.6/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-3.6.asc
+EOF
+
+yum install mongodb-org -y
+
+mongo -v
