143e4bafb93f998496f5cf275e465126ebf2653a
mengjun <813935532@qq.com>
2019-03-06 18:44:14 +0800

    modify yarn.sh

--- yarn.sh
+++ yarn.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
 
 yum install yarn -y
 
