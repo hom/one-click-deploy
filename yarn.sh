ab4a58938ae7010714361dc04567e1dccaa7eb7d
mengjun <813935532@qq.com>
2019-03-06 18:51:25 +0800

    add nodejs source repo

--- yarn.sh
+++ yarn.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
+
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
 
 yum install yarn -y
